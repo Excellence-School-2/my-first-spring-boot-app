5807d1851975c79fc88c4911a446461a7e525740
Mellgood <carlocentos@gmail.com>
2021-11-09 12:53:18 +0100

    Model, repository and bootstrap implemented. Closes #1

---
+++ src/main/java/it/ntt/hellospringboot/bootstrap/DataBootstrap.java
@@ -0,0 +1,36 @@
+package it.ntt.hellospringboot.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import it.ntt.hellospringboot.model.Author;
+import it.ntt.hellospringboot.model.Book;
+import it.ntt.hellospringboot.repository.AuthorRepository;
+import it.ntt.hellospringboot.repository.BookRepository;
+
+@Component
+public class DataBootstrap implements CommandLineRunner{
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    
+    public DataBootstrap(AuthorRepository authorRepository, BookRepository bookRepository){
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author carlo = new Author("Carlo", "Centofanti");
+
+        Book book1 = new Book("Il mio primo libro", "123-abc");
+        book1.setAuthor(carlo);
+
+        authorRepository.save(carlo);
+        bookRepository.save(book1);
+
+        
+        
+    }
+    
+}
