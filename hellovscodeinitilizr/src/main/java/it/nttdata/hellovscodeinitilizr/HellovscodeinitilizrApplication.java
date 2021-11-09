9aec2d417e649eb0f4c6d05ef943541337d20395
Mellgood <carlocentos@gmail.com>
2021-11-09 10:19:45 +0100

    Codice base

---
+++ hellovscodeinitilizr/src/main/java/it/nttdata/hellovscodeinitilizr/HellovscodeinitilizrApplication.java
@@ -0,0 +1,15 @@
+package it.nttdata.hellovscodeinitilizr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellovscodeinitilizrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellovscodeinitilizrApplication.class, args);
+		
+	}
+
+
+}
---
+++ hellovscodeinitilizr/src/test/java/it/nttdata/hellovscodeinitilizr/HellovscodeinitilizrApplicationTests.java
@@ -0,0 +1,13 @@
+package it.nttdata.hellovscodeinitilizr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellovscodeinitilizrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
